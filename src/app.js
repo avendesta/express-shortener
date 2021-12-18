92a7099a8b3a3ba8165500b09b1829a23cbbf206
avendesta <53615807+avendesta@users.noreply.github.com>
2021-12-18 11:12:56 -0600

    Added a User schema and created a sample user in database

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
+// module imports
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+// my module imports
 const config = require("./config");
-
+// initialization
 const app = express();
+// settings and configurations
 app.disable("x-powered-by");
 const port = config.PORT;
-
+// middlewares
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(config.dbURL);
 
-const productSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  price: Number,
-});
-
-const productModel = mongoose.model("Product", productSchema);
-
-const p1 = new productModel({
+const { User } = require("./resources/user/user.model");
+const p1 = new User({
   _id: new mongoose.Types.ObjectId(),
-  name: "Winter Jacket",
-  price: 33,
+  email: "john@mail.com",
+  password: "Winter Jacket",
 });
 
 p1.save()
   .then((r) => console.log(r))
   .catch((e) => console.error("Error ", e));
 
+// routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// run server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
